d842ca447968e1386a45001dfd972373688ea892
David W.H. Swenson <dwhs@hyperblazer.net>
2019-02-07 21:47:21 +0100

    Add _deploy.sh

---
+++ _deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" = "local" ]; then
+    DEPLOY_DIR=./_site  # test deploy
+elif [ "$1" = "deploy" ]; then
+    DEPLOY_DIR=~/public_html/jekyll_site  # actual deploy
+else
+    echo "Requires 'local' or 'deploy' command"
+    exit
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR && git pull && bundle exec jekyll build -I -d $DEPLOY_DIR
