1e3fae0f1095d88a6448b2b308a7504797e33bec
David W.H. Swenson <dwhs@hyperblazer.net>
2024-10-05 17:51:12 -0500

    fix bucket name; test if we do prod run

--- _devtools/staging.sh
+++ _devtools/staging.sh
@@ -1,4 +1,4 @@
 RUNTYPE="$RUNTYPE"
-BUCKETNAME="hb-net-testbucket"
 PREFIX="$PR_NUMBER"
+BUCKETNAME="hb-net-testbucket-186"
 ROLE="arn:aws:iam::730335208220:role/hb-net-bucket-and-role-WorkflowRole-6LcFv4gsMqB7"
